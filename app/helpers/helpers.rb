6de740b94b75743caa2d2377eec877420e8af7d0
Jharell Chambers <jharell.chambers@selu.edu>
2019-12-31 02:13:05 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -5,10 +5,7 @@ class Helpers
   end
   
   def self.is_logged_in?(session)
-    if session[:user_id]
-      return true 
-    else 
-      return false
-    end 
+   true if session[:user_id]
+   
   end
 end
