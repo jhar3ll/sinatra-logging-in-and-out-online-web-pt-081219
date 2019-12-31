a646b7e6f608491eded9e0c34e1cf9b490861d28
Jharell Chambers <jharell.chambers@selu.edu>
2019-12-31 02:02:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,10 @@ class ApplicationController < Sinatra::Base
     if @user
       session[:user_id] = @user.id
       redirect to '/account'
-  end
-
+    else erb :error 
+   end 
+  end 
+  
   get '/account' do
 
   end
